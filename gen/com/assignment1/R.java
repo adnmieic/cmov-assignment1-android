feb74e2015c4760c9394c2f09d923fcaad945f40
Nuno Silva <nufil.silva@gmail.com>
2011-10-04 16:08:37 +0100

    Initial Project Configuration

--- gen/com/assignment1/R.java
+++ gen/com/assignment1/R.java
@@ -13,11 +13,31 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int checkBox1=0x7f050008;
+        public static final int checkBox2=0x7f050009;
+        public static final int imageView1=0x7f050001;
+        public static final int linearLayout1=0x7f050000;
+        public static final int linearLayout2=0x7f050002;
+        public static final int linearLayout3=0x7f05000a;
+        public static final int linearLayout4=0x7f050007;
+        public static final int loginActivityLoginButton=0x7f05000b;
+        public static final int loginActivityPasswordEditText=0x7f050006;
+        public static final int loginActivityUsernameEditText=0x7f050004;
+        public static final int textView1=0x7f050003;
+        public static final int textView2=0x7f050005;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int loginactivity=0x7f030000;
+        public static final int mainmenuactivity=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int autologin=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int login=0x7f040002;
+        public static final int password=0x7f040004;
+        public static final int rememberme=0x7f040005;
+        public static final int username=0x7f040003;
     }
 }
